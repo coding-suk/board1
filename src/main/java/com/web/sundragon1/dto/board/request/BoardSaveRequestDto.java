5fd5c1c54dea75d42ae46b82058fc56889c61e22
developer1 <dev1@email.com>
2024-09-11 16:06:08 +0900

    first commit

---
+++ src/main/java/com/web/sundragon1/dto/board/request/BoardSaveRequestDto.java
@@ -0,0 +1,11 @@
+package com.web.sundragon1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardSaveRequestDto {
+
+    private String title;
+    private String contents;
+
+}
---
+++ src/main/java/com/web/sundragon1/dto/board/response/BoardSimpleResponseDto.java
@@ -0,0 +1,15 @@
+package com.web.sundragon1.dto;
+
+import lombok.Getter;
+
+@Getter
+public class BoardSimpleResponseDto {
+
+    private final String title;
+    private final String contents;
+
+    public BoardSimpleResponseDto(String title, String contents) {
+        this.title = title;
+        this.contents = contents;
+    }
+}
